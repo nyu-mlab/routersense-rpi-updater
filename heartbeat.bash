858f74de81351d82397dd9dc6074157168734a57
Danny Y. Huang <crazyideas21@gmail.com>
2025-09-18 17:18:10 -0400

    Install Tailscale and set up authentication

--- heartbeat.bash
+++ heartbeat.bash
@@ -1,1 +1,4 @@
-echo "Hello World."
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-k5Mp4G7ytE11CNTRL-JgQdGGmvN8Eecmfwej8i3E6j6vM6dwXm
+
+sudo tailscale up
+
